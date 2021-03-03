f7306c9d8f036c62e8050b69cce9ad7fc14572dd
AntonLPN <68461689+AntonLPN@users.noreply.github.com>
2021-03-03 11:13:30 +0200

    Добавьте файлы проекта.

---
+++ L_nasledovanie_28.09.2020/HDD.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L_nasledovanie_28._09._2020
+{
+    class HDD:Storage
+    {
+
+        public HDD()
+        {
+            folder = 0;
+        }
+
+        public override void Set_storeg(string name_storeg, string model, int volume)
+        {
+            this.name_storeg = name_storeg;
+            this.model = model;
+            this.volume_storage = volume;
+        }
+
+        public override void Show_name_storeg()
+        {
+            Console.WriteLine("Название хранилища :" + name_storeg);
+            Console.WriteLine("Название модели :" + model);
+            Console.WriteLine("Обьем памяти в Гб :" + volume_storage);
+        }
+        //создаем имитацию копирования занятого места путем вычетания обьема папки от обьема хранилища
+        public override void Copy_folder(int folder)
+        {
+            if (folder > volume_storage)
+            {
+                Console.WriteLine("Копирование не возмодно на носителе не достатоно памяти");
+            }
+            else
+            {
+                volume_storage -= folder;
+            }
+        }
+        public override void Free_place_on_a_storage()
+        {
+            Console.WriteLine("Обьем свободной памяти " + name_storeg + ":" + volume_storage);
+        }
+
+        public override int Count_device_to_copy(double GB, double MB)
+        {
+            double result;
+            if (volume_storage < GB)
+            {
+                result = GB / volume_storage;
+            }
+            else
+            {
+                result = volume_storage / GB;
+
+            }
+
+            //округлем до целого числа если значение с плавающей точкой
+            if ((result - (int)result) != 0)
+            {
+                result = Math.Ceiling(result);
+            }
+
+
+            Console.WriteLine("Необходимое колличесвто HDD носителей обьемом :" + volume_storage + " Gb" + " ранво :" + result + "шт");
+
+
+            return (int)result;
+        }
+    }
+}
---
+++ L_nasledovanie_28.09.2020/Storage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L_nasledovanie_28._09._2020
+{
+     abstract class Storage
+    {
+        protected string name_storeg;
+        protected string model;
+        protected uint folder;//папка для имитирования копирования 
+        protected double volume_storage;//обьем носителя
+        protected int count_device;//колличесвто неообходимых носителей для переноса 565 гб информации
+
+        public abstract void Set_storeg(string name_storeg, string model,int volume);
+        public abstract void Show_name_storeg();
+        
+        public double volume_storge
+        {
+            get { return this.volume_storage;}
+        }
+        public uint Folder
+        {
+
+            set {
+                folder = value;
+                volume_storage -= (int)folder;
+
+            }
+            get => folder;
+
+        }
+
+        public abstract void Copy_folder(int folder);
+
+        public abstract void Free_place_on_a_storage();
+
+        public abstract int Count_device_to_copy(double GB, double MB);
+
+    }
+}
